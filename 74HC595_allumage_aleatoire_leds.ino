29c0930a92be9710655fc24d7975efa98db2989a
Exau_KIM <92597014+RS-malik-el@users.noreply.github.com>
2024-01-30 10:47:50 +0000

    Update 74HC595_allumage_aleatoire_leds.ino

--- 74HC595_allumage_aleatoire_leds.ino
+++ 74HC595_allumage_aleatoire_leds.ino
@@ -1,6 +1,7 @@
 /*
 	DATE 	: 20/04/2022
-	AUTHEUR : RACHEL SYSTEME 
+ 	email : openprogramming23@gmail.com
+	AUTEUR : Exaucé KIMBEMBE / OpenProgramming
 	YOUTUBE : https://youtu.be/VaY-VGB0h5g
 
 	Ce programme permet d'allumer un ensemble de 8 leds via un
